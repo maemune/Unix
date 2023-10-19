6816bd3694f0ecb4c47bdfa64d8dea25ef49b303
maemune <115972024+maemune@users.noreply.github.com>
2023-10-19 17:20:25 +0900

    Create init-setup.sh

---
+++ init-setup.sh
@@ -0,0 +1,19 @@
+#!/bin/bash
+echo '#!/bin/bash
+apt-get update
+apt -y full-upgrade
+apt -y autoremove
+' > /root/Update.sh && chmod u+x /root/Update.sh
+
+crontab -l > {tmpfile}
+echo "0 */12 * * * /root/Update.sh" >> {tmpfile}
+crontab {tmpfile}
+rm {tmpfile}
+
+/root/Update.sh
+cp /etc/default/grub /etc/default/grub.AutoBackup
+sed -i -e 's/GRUB_TIMEOUT=5/GRUB_TIMEOUT=0/g' /etc/default/grub
+sed -i -e 's/GRUB_CMDLINE_LINUX_DEFAULT="quiet"/GRUB_CMDLINE_LINUX_DEFAULT="quiet intel_iommu=on"/g' /etc/default/grub
+update-grub
+
+reboot now
