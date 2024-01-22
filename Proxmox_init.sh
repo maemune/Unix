ee7e2f0bf6b532c00c75de3e7f126e50c5e243fe
maemune <115972024+maemune@users.noreply.github.com>
2024-01-22 11:34:37 +0900

    Update Proxmox_init.sh

--- Proxmox_init.sh
+++ Proxmox_init.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-#wget https://raw.githubusercontent.com/maemune/Unix/main/Proxmox_init.sh && nano ./Proxmox_init.sh && chmod u+x ./Proxmox_init && ./Proxmox_init
+#wget https://raw.githubusercontent.com/maemune/Unix/main/Proxmox_init.sh && nano ./Proxmox_init.sh && chmod u+x ./Proxmox_init.sh  && ./Proxmox_init
 
 echo '#!/bin/bash
 apt-get update
