0dbabad44c97996e7d80e676fb5630e2f43e1b21
maemune <115972024+maemune@users.noreply.github.com>
2024-07-08 17:27:22 +0900

    Update Generat_SSH.sh

--- Generat_SSH.sh
+++ Generat_SSH.sh
@@ -1,4 +1,4 @@
 #!/bin/bash
-
+#wget https://raw.githubusercontent.com/maemune/Unix/main/Generat_SSH.sh && nano ./Generat_SSH.sh && chmod u+x ./Generat_SSH.sh && ./Generat_SSH.sh
 file_name=$(hostname)
 echo "y" | ssh-keygen -t ed25519 -f ~/.ssh/$file_name -N "" -C "" -q
