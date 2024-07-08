640d6cba42e868efd1a208551322d673691de1f1
maemune <115972024+maemune@users.noreply.github.com>
2024-07-08 17:19:15 +0900

    Update and rename Ubuntu_Update.sh to Update.sh

---
+++ Update.sh
@@ -0,0 +1,11 @@
+#!/bin/bash
+#wget https://raw.githubusercontent.com/maemune/Unix/main/Update.sh && nano ./Update.sh && chmod u+x ./Update.sh && ./Update.sh
+
+# Setting you info
+#PASSWORD=""
+
+# Update
+sudo apt-get update
+sudo apt -y full-upgrade
+sudo apt -y autoremove
+sudo apt -y clean
