75a783f6c59501507383a0fbcef755397cd71337
maemune <115972024+maemune@users.noreply.github.com>
2024-07-09 17:06:09 +0900

    Update Python_Install.sh

--- Python_Install.sh
+++ Python_Install.sh
@@ -1,21 +1,31 @@
 #!/bin/bash
+#wget https://raw.githubusercontent.com/maemune/Unix/main/Python_Install.sh && nano ./Python_Install.sh && chmod u+x ./Python_Install.sh && ./Python_Install.sh
 
 # 必要なパッケージのインストール
-sudo apt install -y software-properties-common
+sudo apt update && sudo apt install -y software-properties-common
+
+# deadsnakes PPAの追加
 echo "" | sudo add-apt-repository ppa:deadsnakes/ppa
 sudo apt update
+
+# Python 3.12のインストール
 sudo apt install -y python3.12 python3.12-dev
 
 # 必要な開発ツールのインストール
 sudo apt-get install -y libreadline-dev zlib1g-dev libncursesw5-dev libssl-dev libsqlite3-dev libgdbm-dev libc6-dev libbz2-dev
 sudo apt-get install -y build-essential libffi-dev libexpat1-dev liblzma-dev python3-testresources
 
-# システムパッケージ制限を無視してpipをインストール
-curl -sS https://bootstrap.pypa.io/get-pip.py | sudo python3.12 --break-system-packages
+# pipのインストール
+curl -sS https://bootstrap.pypa.io/get-pip.py -o get-pip.py
+sudo python3.12 get-pip.py --break-system-packages
 sudo python3.12 -m pip install --upgrade pip --break-system-packages
+rm get-pip.py
 
 # 必要に応じてpipコマンドを移動
 sudo mv /usr/local/bin/pip /usr/local/bin/pip_ && sudo mv /usr/local/bin/pip3.12 /usr/local/bin/pip
-echo 'export PATH=$PATH:/usr/local/bin' >> ~/.profile && source ~/.profile
+
+# 環境変数の更新
+echo 'export PATH=$PATH:/usr/local/bin' >> ~/.profile
+source ~/.profile
 
 exit
