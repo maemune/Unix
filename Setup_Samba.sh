2d71c50548f8e1fd16c7c8fb3c49311aa2de51b7
maemune <115972024+maemune@users.noreply.github.com>
2025-06-04 03:39:31 +0900

    Update Setup_Samba.sh

--- Setup_Samba.sh
+++ Setup_Samba.sh
@@ -1,5 +1,6 @@
 #!/bin/bash
-#https://raw.githubusercontent.com/maemune/Unix/refs/heads/main/Setup_Samba.sh
+#wget https://raw.githubusercontent.com/maemune/Unix/refs/heads/main/Setup_Samba.sh && nano ./Setup_Samba.sh && chmod u+x ./Setup_Samba.sh && ./Setup_Samba.sh
+
 sudo apt install -y samba
 sudo cp /etc/samba/smb.conf /etc/samba/smb.conf.bak
 sudo nano /etc/samba/smb.conf
