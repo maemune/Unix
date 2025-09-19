1bd2cfb61cd3ad80bc4988d988348cd4573de310
maemune <115972024+maemune@users.noreply.github.com>
2025-09-19 22:46:17 +0900

    Update Refresh_host.sh

--- Refresh_host.sh
+++ Refresh_host.sh
@@ -1,14 +1,13 @@
 #!/bin/bash
 
-#wget https://raw.githubusercontent.com/maemune/Unix/main/Refresh_host.sh && nano ./Refresh_host.sh && chmod u+x ./Refresh_host.sh && ./Refresh_host.sh
-
 get_input_with_confirmation() {
     local input_var=$1
     local prompt=$2
 
     while true; do
         echo -n "$prompt: "
-        read -r "$input_var"
+        read -r input
+        eval $input_var=\$input
 
         echo "Entered $prompt: ${!input_var}"
 
@@ -24,13 +23,41 @@ get_input_with_confirmation() {
 }
 
 get_input_with_confirmation hostname "Please enter your new hostname"
-get_input_with_confirmation ip_address "Please enter your new IP address"
+get_input_with_confirmation ip_address "Please enter your new IP address (e.g. 192.168.1.111)"
+get_input_with_confirmation gateway "Please enter your gateway (e.g. 192.168.1.1)"
+get_input_with_confirmation dns "Please enter your DNS servers (comma separated, e.g. 8.8.8.8,1.1.1.1)"
 
-sudo hostnamectl set-hostname $hostname
+if [[ ! "$ip_address" =~ /[0-9]+$ ]]; then
+    ip_address="$ip_address/24"
+fi
+
+sudo hostnamectl set-hostname "$hostname"
 
 netplan_file="/etc/netplan/50-cloud-init.yaml"
-cur_ip=$(ip addr show | awk '/inet 192/ {gsub(/\/.*/, "", $2); print $2}')
-sudo sed -i "s/$cur_ip/$ip_address/g" "$netplan_file"
+iface=$(ip -o -4 route show to default | awk '{print $5}')
+
+if grep -q "dhcp4: true" "$netplan_file"; then
+    echo "Current config: DHCP. Switching to static..."
+    sudo tee "$netplan_file" > /dev/null <<EOF
+network:
+  version: 2
+  ethernets:
+    $iface:
+      dhcp4: false
+      addresses:
+        - $ip_address
+      gateway4: $gateway
+      nameservers:
+        addresses: [$dns]
+EOF
+else
+    echo "Current config: Static. Updating IP..."
+    sudo sed -i "s|addresses:.*|addresses:\n        - $ip_address|" "$netplan_file"
+    sudo sed -i "s|gateway4:.*|gateway4: $gateway|" "$netplan_file"
+    sudo sed -i "s|addresses: \[.*\]|addresses: [$dns]|" "$netplan_file"
+fi
 
 sudo netplan apply
+echo "Rebooting in 1 seconds..."
+sleep 1
 sudo reboot now
