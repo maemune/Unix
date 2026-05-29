edb26022970853df2303abe8fd28161efc4cd6ae
maemune <115972024+maemune@users.noreply.github.com>
2026-05-29 14:01:00 +0900

    Add RockyLinux_Update.sh script for system updates

---
+++ RockyLinux/RockyLinux_Update.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#wget https://raw.githubusercontent.com/maemune/Unix/main/RockyLinux_Update.sh && nano ./RockyLinux_Update.sh && chmod u+x ./RockyLinux_Update.sh && ./RockyLinux_Update.sh
+# Setting your info
+#PASSWORD=""
+
+# Update
+sudo dnf check-update
+sudo dnf -y upgrade
+sudo dnf -y autoremove
+sudo dnf clean all
