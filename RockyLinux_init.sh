e5322b21f2b99a67030e795a95a8bf94b3d00617
maemune <115972024+maemune@users.noreply.github.com>
2026-05-29 11:33:24 +0900

    Modify RockyLinux_init.sh to include external scripts
    Updated script to download and set permissions for external scripts.

--- RockyLinux_init.sh
+++ RockyLinux_init.sh
@@ -67,15 +67,14 @@ sudo chmod 600 /home/${USERNAME}/.ssh/authorized_keys
 sudo chown -R ${USERNAME}:${USERNAME} /home/${USERNAME}/.ssh
 
 # External scripts
-#wget -q https://raw.githubusercontent.com/maemune/Unix/main/Generat_SSH.sh
-#chmod u+x Generat_SSH.sh
-#./Generat_SSH.sh
+wget -q https://raw.githubusercontent.com/maemune/Unix/main/Generat_SSH.sh
+chmod u+x /home/${USERNAME}/Generat_SSH.sh
 
-#wget -q https://raw.githubusercontent.com/maemune/Unix/main/Refresh_host.sh
-#chmod u+x Refresh_host.sh
+wget -q https://raw.githubusercontent.com/maemune/Unix/main/Refresh_host.sh
+chmod u+x /home/${USERNAME}/Refresh_host.sh
 
 wget -q https://raw.githubusercontent.com/maemune/Unix/refs/heads/main/RockyLinux_Update.sh
-sudo chown ${USERNAME}:${USERNAME} /home/${USERNAME}/RockyLinux_Update.sh && chmod u+x /home/${USERNAME}/RockyLinux_Update.sh
+sudo chmod u+x /home/${USERNAME}/RockyLinux_Update.sh
 
 # Cron
 TMPFILE="$(mktemp)"
